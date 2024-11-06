d47ff6f5cf5ac5ff9024c828c1a96961b350c7aa
Alcir Castro <121054793+AlcirCastro@users.noreply.github.com>
2024-11-06 19:09:25 -0400

    Add files via upload

--- PlainText/app/src/main/java/com/example/plaintext/data/dao/PasswordDao.kt
+++ PlainText/app/src/main/java/com/example/plaintext/data/dao/PasswordDao.kt
@@ -1,8 +1,23 @@
 package com.example.plaintext.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.plaintext.data.model.Password
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-abstract class PasswordDao : BaseDao<Password> {
+interface PasswordDao {
+    @Query("SELECT * FROM passwords")
+    fun getAllPasswords(): Flow<List<Password>>
+
+    @Insert
+    suspend fun addPassword(password: Password): Long
+
+    @Update
+    suspend fun updatePassword(password: Password)
+
+    @Query("SELECT * FROM passwords WHERE id = :id")
+    suspend fun getPasswordById(id: Int): Password?
 }
