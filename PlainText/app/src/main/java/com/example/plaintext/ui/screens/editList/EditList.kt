0ab8b6f63a57667d5a3c1da6936eb50cbcd589ee
Pedro Lucas Bezerra Mendes <104802373+PedroLucasMendes@users.noreply.github.com>
2024-11-06 15:16:05 -0400

    editlist composable

--- PlainText/app/src/main/java/com/example/plaintext/ui/screens/editList/EditList.kt
+++ PlainText/app/src/main/java/com/example/plaintext/ui/screens/editList/EditList.kt
@@ -3,6 +3,7 @@ package com.example.plaintext.ui.screens.editList
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -27,7 +29,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.plaintext.data.model.PasswordInfo
@@ -52,6 +56,79 @@ fun EditList(
     savePassword: (password: PasswordInfo) -> Unit
 ) {
 
+    val stateSenha = EditListState(
+        nomeState = rememberSaveable {mutableStateOf(args.password.name)} ,
+        usuarioState = rememberSaveable {mutableStateOf(args.password.login)},
+        senhaState = rememberSaveable {mutableStateOf(args.password.password)},
+        notasState = rememberSaveable {mutableStateOf(args.password.notes)}
+    )
+
+    Scaffold(
+
+        topBar = {
+            Box(
+                Modifier
+                    .background(Color(0xFF2C1B16))
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp, horizontal = 10.dp)
+            ){
+                Text("Plain Text", color = Color.White,fontSize = 20.sp,)
+            }
+        },
+
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .background(Color(0xFF2C1B16)), // Fundo marrom escuro
+
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Box(
+                    modifier = Modifier
+                        .background(Color(0xFFB2EB40))
+                        .fillMaxWidth()
+                        .padding(horizontal = 30.dp, vertical = 16.dp)
+
+                ){
+                    Text("Adicionar nova Senha",
+                        color = Color.White,
+                        fontSize = 20.sp,
+
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                EditInput("Nome", textInputState = stateSenha.nomeState)
+                EditInput("Usuário",  textInputState = stateSenha.usuarioState)
+                EditInput("Senha",  textInputState = stateSenha.senhaState)
+                EditInput("Notas", textInputHeight = 100,  textInputState = stateSenha.notasState)
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Button(
+                    onClick = {
+                        // Aqui vai o código para salvar a senha
+                    },
+
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFFFFA26B), // Cor de fundo do botão
+                        contentColor = Color(0xFF4A2C1F)    // Cor do texto do botão
+                    ),
+
+                    modifier = Modifier
+                        .padding(16.dp)
+
+                ) {
+                    Text("Salvar", fontSize = 16.sp)
+                }
+            }
+        }
+    )
 }
 
 
@@ -75,10 +152,11 @@ fun EditInput(
         OutlinedTextField(
             value = textState,
             onValueChange = { textState = it },
-            label = { Text(textInputLabel) },
+            label = { Text(textInputLabel, color = Color.White)},
             modifier = Modifier
                 .height(textInputHeight.dp)
-                .fillMaxWidth()
+                .fillMaxWidth(),
+            textStyle = TextStyle(color = Color.White)
         )
 
     }
@@ -89,7 +167,7 @@ fun EditInput(
 @Composable
 fun EditListPreview() {
     EditList(
-        Screen.EditList(PasswordInfo(1, "Nome", "Usuário", "Senha", "Notas")),
+        Screen.EditList(PasswordInfo(1, "", "", "", "")),
         navigateBack = {},
         savePassword = {}
     )
