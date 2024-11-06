3ee5e14af45eaff661ce018a9e9ebb8e4a7226f4
Alcir Castro <alcir.figueiredo@icomp.ufam.edu.br>
2024-11-06 18:48:15 -0400

    att tela login

--- PlainText/app/src/main/java/com/example/plaintext/ui/screens/login/Login.kt
+++ PlainText/app/src/main/java/com/example/plaintext/ui/screens/login/Login.kt
@@ -1,41 +1,17 @@
 package com.example.plaintext.ui.screens.login
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,26 +19,18 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.plaintext.R
+import com.example.plaintext.ui.viewmodel.LoginViewModel
 import com.example.plaintext.ui.viewmodel.PreferencesViewModel
-
-data class LoginState(
-    val preencher: Boolean,
-    val login: String,
-    val navigateToSettings: () -> Unit,
-    val navigateToList: (name: String) -> Unit,
-    val checkCredentials: (login: String, password: String) -> Boolean,
-)
 
 @Composable
 fun Login_screen(
     navigateToSettings: () -> Unit,
     navigateToList: () -> Unit,
-    //viewModel: PreferencesViewModel = hiltViewModel()
+    loginViewModel: LoginViewModel = hiltViewModel(),
+    preferencesViewModel: PreferencesViewModel = hiltViewModel()
 ) {
-
-    var login by remember { mutableStateOf("") }
-    var senha by remember { mutableStateOf("") }
-    var checked by remember { mutableStateOf(false) }
+    val state = loginViewModel.viewState
+    val context = LocalContext.current
 
     Scaffold(
         topBar = { TopBarComponent(navigateToSettings = navigateToSettings) }
@@ -79,7 +47,7 @@ fun Login_screen(
             Row(
                 modifier = Modifier
                     .height(150.dp)
-                    .width(5000.dp)
+                    .fillMaxWidth()
                     .background(Color.Green),
                 verticalAlignment = Alignment.CenterVertically
             ) {
@@ -110,26 +78,23 @@ fun Login_screen(
             Spacer(modifier = Modifier.height(60.dp))
 
             OutlinedTextField(
-                value = login,
-                onValueChange = { login = it },
-                label = { Text(
-                    text = "Nome de Usuário",
-                    color = Color.White,
-                ) },
+                value = state.login,
+                onValueChange = { loginViewModel.onLoginChange(it) },
+                label = { Text(text = "Nome de Usuário", color = Color.White) },
+                textStyle = LocalTextStyle.current.copy(color = Color.White), // Define a cor do texto preenchido como branca
                 modifier = Modifier.fillMaxWidth()
             )
 
             Spacer(modifier = Modifier.height(18.dp))
 
             OutlinedTextField(
-                value = senha,
-                onValueChange = { senha = it },
-                label = { Text(
-                    text = "Senha",
-                    color = Color.White,
-                ) },
+                value = state.senha,
+                onValueChange = { loginViewModel.onSenhaChange(it) },
+                label = { Text(text = "Senha", color = Color.White) },
+                textStyle = LocalTextStyle.current.copy(color = Color.White), // Define a cor do texto preenchido como branca
                 visualTransformation = PasswordVisualTransformation(),
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
             )
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -141,8 +106,8 @@ fun Login_screen(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Checkbox(
-                    checked = checked,
-                    onCheckedChange = { checked = it }
+                    checked = state.checked,
+                    onCheckedChange = { loginViewModel.onCheckedChange(it) }
                 )
                 Text(
                     text = "Salvar as informações de login",
@@ -156,9 +121,12 @@ fun Login_screen(
 
             Button(
                 onClick = {
-                    if (login.isNotEmpty() && senha.isNotEmpty()) {
-                        navigateToList() // Navigate to the list
-                    }
+                    loginViewModel.checkCredentials(
+                        context,
+                        navigateToList,
+                        savedLogin = preferencesViewModel.preferencesState.login,
+                        savedPassword = preferencesViewModel.preferencesState.password
+                    )
                 },
                 modifier = Modifier
                     .width(100.dp)
@@ -168,7 +136,6 @@ fun Login_screen(
             }
         }
     }
-
 }
 
 @Preview(showBackground = true)
@@ -177,11 +144,8 @@ fun PreviewUI() {
     Login_screen(
         navigateToSettings = {},
         navigateToList = {}
-        //viewModel = mockViewModel
     )
 }
-
-
 
 @Composable
 fun MyAlertDialog(shouldShowDialog: MutableState<Boolean>) {
@@ -190,7 +154,6 @@ fun MyAlertDialog(shouldShowDialog: MutableState<Boolean>) {
             onDismissRequest = {
                 shouldShowDialog.value = false
             },
-
             title = { Text(text = "Sobre") },
             text = { Text(text = "PlainText Password Manager v1.0") },
             confirmButton = {
@@ -207,8 +170,8 @@ fun MyAlertDialog(shouldShowDialog: MutableState<Boolean>) {
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 fun TopBarComponent(
-    navigateToSettings: (() -> Unit?)? = null,
-    navigateToSensores: (() -> Unit?)? = null,
+    navigateToSettings: (() -> Unit)? = null,
+    navigateToSensores: (() -> Unit)? = null,
 ) {
     var expanded by remember { mutableStateOf(false) }
     val shouldShowDialog = remember { mutableStateOf(false) }
@@ -220,35 +183,45 @@ fun TopBarComponent(
     TopAppBar(
         title = { Text("PlainText") },
         actions = {
-            if (navigateToSettings != null && navigateToSensores != null) {
-                IconButton(onClick = { expanded = true }) {
-                    Icon(Icons.Default.MoreVert, contentDescription = "Menu")
-                }
-                DropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false }
-                ) {
+            // Botão de três pontos para abrir o menu de opções
+            IconButton(onClick = { expanded = true }) {
+                Icon(Icons.Default.MoreVert, contentDescription = "Menu")
+            }
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                // Opção para Configurações
+                DropdownMenuItem(
+                    text = { Text("Preferências") },
+                    onClick = {
+                        navigateToSettings?.invoke()
+                        expanded = false
+                    },
+                    modifier = Modifier.padding(8.dp)
+                )
+                // Opção para Sensores, se `navigateToSensores` não for nulo
+                navigateToSensores?.let {
                     DropdownMenuItem(
-                        text = { Text("Configurações") },
+                        text = { Text("Sensores") },
                         onClick = {
-                            navigateToSettings();
-                            expanded = false;
+                            it()
+                            expanded = false
                         },
                         modifier = Modifier.padding(8.dp)
                     )
-                    DropdownMenuItem(
-                        text = {
-                            Text("Sobre");
-                        },
-                        onClick = {
-                            shouldShowDialog.value = true;
-                            expanded = false;
-                        },
-                        modifier = Modifier.padding(8.dp)
-                    )
                 }
+                // Opção para exibir o diálogo "Sobre"
+                DropdownMenuItem(
+                    text = { Text("Sobre") },
+                    onClick = {
+                        shouldShowDialog.value = true
+                        expanded = false
+                    },
+                    modifier = Modifier.padding(8.dp)
+                )
             }
         }
     )
 }
-
